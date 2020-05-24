29d8de9c5a43246a61aa52dcb97796bddf3676fe
jayson_kesterson <your_email@example.com>
2020-05-24 17:34:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+function createEmployeeRecord(employees) {
+    return {
+        firstName: employees[0],
+        familyName: employees[1],
+        title: employees[2],
+        payPerHour: employees[3],
+        timeInEvents: [],
+        timeOutEvents:[]
+    }
+}
+
+function createEmployeeRecords(employeesArr) {
+    return employeesArr.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(employee, time) {
+    employee.timeInEvents.push(
+        {
+           type: 'timeIn',
+           hour: parseInt(time.slice(-4), 10),
+           date: time.slice(0, 10)
+        }
+    )
+    return employee
+}
+
+function createTimeOutEvent(employee, time) {
+    employee.timeOutEvents.push(
+        {
+           type: 'TimeOut',
+           hour: parseInt(time.slice(-4), 10),
+           date: time.slice(0, 10)
+        }
+    )
+    return employee
+}
+
+function hoursWorkedOnDate(employee, date) {
+    let timeIn = employee.timeInEvents.find(workDay => workDay.date === date)
+    let timeOut = employee.timeOutEvents.find(workDay => workDay.date === date)
+    return timeOut - timeIn
+}
